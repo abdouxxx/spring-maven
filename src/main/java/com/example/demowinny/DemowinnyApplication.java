30c314b71f221fce23f2b0bf991cf63f4e59c243
abdouxxx <abdoulayebw@gmail.com>
2026-06-22 04:38:35 +0000

    Premier commit Spring Boot

---
+++ src/main/java/com/example/demowinny/DemowinnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowinny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowinnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowinnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demowinny/DemowinnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demowinny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowinnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
